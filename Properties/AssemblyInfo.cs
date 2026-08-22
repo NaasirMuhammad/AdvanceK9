1c0118aa7b71a33170b150475b3a6c05a0fe56f7
NaasirMuhammad <141694761+NaasirMuhammad@users.noreply.github.com>
2026-08-22 08:10:01 -0400

    Release v1.7.2-beta: Properties/AssemblyInfo.cs

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -2,10 +2,10 @@ using System.Reflection;
 using Rage.Attributes;
 
 [assembly: AssemblyTitle("Advanced K9")]
-[assembly: AssemblyDescription("Advanced police K9 partner for LSPDFR")]
+[assembly: AssemblyDescription("Advanced police K9 partner for LSPDFR (Beta)")]
 [assembly: AssemblyCompany("AdvancedK9 Community Project")]
-[assembly: AssemblyProduct("Advanced K9")]
-[assembly: AssemblyVersion("1.7.1.0")]
-[assembly: AssemblyFileVersion("1.7.1.0")]
-[assembly: AssemblyInformationalVersion("1.7.1+compact-eup-menu-vest-preview")]
-[assembly: Plugin("Advanced K9", Description = "Advanced police K9 partner", Author = "AdvancedK9 Community Project")]
+[assembly: AssemblyProduct("Advanced K9 Beta")]
+[assembly: AssemblyVersion("1.7.2.0")]
+[assembly: AssemblyFileVersion("1.7.2.0")]
+[assembly: AssemblyInformationalVersion("1.7.2-beta+ini-voice-key")]
+[assembly: Plugin("Advanced K9 Beta", Description = "Advanced police K9 partner beta", Author = "AdvancedK9 Community Project")]
