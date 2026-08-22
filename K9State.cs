7bcffadfea6e4e07317809d520f57032c4626591
NaasirMuhammad <141694761+NaasirMuhammad@users.noreply.github.com>
2026-08-21 20:51:12 -0400

    Add K9State.cs from v1.6 checkpoint

---
+++ K9State.cs
@@ -0,0 +1,5 @@
+namespace AdvancedK9
+{
+    internal enum K9State { Dismissed, Following, Heeling, Sitting, Lying, Staying, Guarding, Searching, Tracking, Apprehending, Fetching, InVehicle, Leashed, Academy, Injured }
+    internal enum K9Command { SpawnDismiss, Follow, Heel, Sit, LieDown, Stay, Recall, Fetch, SearchArea, SearchVehicle, Track, Apprehend, Release, Guard, Bark, EnterVehicle, ExitVehicle, Pet, Feed, Inspect, FirstAid, ToggleLeash, ToggleCamera, Training }
+}
