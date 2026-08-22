1aa754b8c4943c7506831328c299b3b0fd495d20
NaasirMuhammad <141694761+NaasirMuhammad@users.noreply.github.com>
2026-08-22 16:08:23 -0400

    v0.20-beta care: update Properties/AssemblyInfo.cs

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -7,5 +7,5 @@ using Rage.Attributes;
 [assembly: AssemblyProduct("Advanced K9 Beta")]
 [assembly: AssemblyVersion("0.20.0.0")]
 [assembly: AssemblyFileVersion("0.20.0.0")]
-[assembly: AssemblyInformationalVersion("0.20-beta+specialty-detection-academy")]
+[assembly: AssemblyInformationalVersion("0.20-beta+care-needs-petting-specialties")]
 [assembly: Plugin("Advanced K9 Beta", Description = "Advanced police K9 partner beta", Author = "AdvancedK9 Community Project")]
