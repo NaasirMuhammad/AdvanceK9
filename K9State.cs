e9408f5a6d73351480d06d359e3a481c28b1bcfd
NaasirMuhammad <141694761+NaasirMuhammad@users.noreply.github.com>
2026-08-22 15:51:56 -0400

    v0.20-beta specialties: update K9State.cs

--- K9State.cs
+++ K9State.cs
@@ -1,5 +1,6 @@
 namespace AdvancedK9
 {
     internal enum K9State { Dismissed, Following, Heeling, Sitting, Lying, Staying, Guarding, Searching, Tracking, Apprehending, Fetching, InVehicle, Leashed, Academy, Injured }
-    internal enum K9Command { SpawnDismiss, Follow, Heel, Sit, LieDown, Stay, Recall, Fetch, SearchArea, SearchVehicle, Track, Apprehend, Release, Guard, Bark, EnterVehicle, ExitVehicle, Pet, Feed, Inspect, FirstAid, ToggleLeash, ToggleCamera, Training }
+    internal enum DetectionSpecialty { General, Narcotics, Explosives, Weapons }
+    internal enum K9Command { SpawnDismiss, Follow, Heel, Sit, LieDown, Stay, Recall, Fetch, SearchArea, SearchVehicle, SearchNarcotics, SearchExplosives, SearchWeapons, Track, Apprehend, Release, Guard, Bark, EnterVehicle, ExitVehicle, Pet, Feed, Inspect, FirstAid, ToggleLeash, ToggleCamera, Training, TrainNarcotics, TrainExplosives, TrainWeapons }
 }
