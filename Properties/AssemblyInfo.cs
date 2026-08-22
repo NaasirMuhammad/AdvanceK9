d470cd26255f1f0deba80edc03e216f0502a4ffc
NaasirMuhammad <141694761+NaasirMuhammad@users.noreply.github.com>
2026-08-21 20:51:36 -0400

    Add Properties/AssemblyInfo.cs from v1.6 checkpoint

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+using System.Reflection;
+using Rage.Attributes;
+
+[assembly: AssemblyTitle("Advanced K9")]
+[assembly: AssemblyDescription("Advanced police K9 partner for LSPDFR")]
+[assembly: AssemblyCompany("AdvancedK9 Community Project")]
+[assembly: AssemblyProduct("Advanced K9")]
+[assembly: AssemblyVersion("1.6.0.0")]
+[assembly: AssemblyFileVersion("1.6.0.0")]
+[assembly: AssemblyInformationalVersion("1.6.0+source-rebuild")]
+[assembly: Plugin("Advanced K9", Description = "Advanced police K9 partner", Author = "AdvancedK9 Community Project")]
