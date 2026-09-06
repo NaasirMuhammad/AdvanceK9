bebd071d1fb81b21987556387c830e18ae068c92
NaasirMuhammad <141694761+NaasirMuhammad@users.noreply.github.com>
2026-09-06 12:39:39 -0400

    Credit mtennekten as AdvancedK9 owner

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -3,9 +3,9 @@ using Rage.Attributes;
 
 [assembly: AssemblyTitle("Advanced K9")]
 [assembly: AssemblyDescription("Advanced police K9 partner for LSPDFR (Beta)")]
-[assembly: AssemblyCompany("Naasir Muhammad")]
+[assembly: AssemblyCompany("mtennekten")]
 [assembly: AssemblyProduct("Advanced K9 Beta")]
 [assembly: AssemblyVersion("0.24.0.0")]
 [assembly: AssemblyFileVersion("0.24.0.0")]
 [assembly: AssemblyInformationalVersion("0.24.0-beta")]
-[assembly: Plugin("Advanced K9 Beta", Description = "Advanced police K9 partner beta", Author = "Naasir Muhammad")]
+[assembly: Plugin("Advanced K9 Beta", Description = "Advanced police K9 partner beta", Author = "mtennekten")]
