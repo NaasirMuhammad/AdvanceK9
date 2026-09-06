0e839731789f292e5562b54b65cb85b1a45b8d03
NaasirMuhammad <141694761+NaasirMuhammad@users.noreply.github.com>
2026-09-06 12:39:51 -0400

    Credit mtennekten as AdvancedK9 owner

--- Callouts/Properties/AssemblyInfo.cs
+++ Callouts/Properties/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 using System.Reflection;
 [assembly: AssemblyTitle("AdvancedK9 Dynamic Callouts")]
 [assembly: AssemblyDescription("Isolated LSPDFR callout expansion for AdvancedK9 Beta")]
-[assembly: AssemblyCompany("AdvancedK9 Community Project")]
+[assembly: AssemblyCompany("mtennekten")]
 [assembly: AssemblyProduct("AdvancedK9 Callouts Beta")]
 [assembly: AssemblyVersion("0.24.0.0")]
 [assembly: AssemblyFileVersion("0.24.0.0")]
