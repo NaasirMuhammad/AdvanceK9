1a360bc0852f68159492f8bfb814aab3849b545d
NaasirMuhammad <141694761+NaasirMuhammad@users.noreply.github.com>
2026-09-06 13:46:36 -0400

    Add AdvancedK9 callout selection menu

--- Callouts/EntryPoint.cs
+++ Callouts/EntryPoint.cs
@@ -1,6 +1,7 @@
 using System;
 using System.IO;
 using System.Reflection;
+using System.Windows.Forms;
 using AdvancedK9.API;
 using LSPD_First_Response.Mod.API;
 using Rage;
@@ -10,11 +11,14 @@ namespace AdvancedK9.Callouts
     public sealed class EntryPoint : Plugin
     {
         private static bool _registered;
+        private static bool _running;
+        private static bool _menuOpen;
         private static ResolveEventHandler _dependencyResolver;
 
         public override void Initialize()
         {
             InstallDependencyResolver();
+            _running=true;
             GameFiber.StartNew(()=>{
                 GameFiber.Wait(2500);
                 if(_registered)return;
@@ -25,6 +29,41 @@ namespace AdvancedK9.Callouts
                 _registered=count>0;
                 Game.LogTrivial("AdvancedK9 Callouts: registered "+count+"/3 callouts after LSPDFR duty initialization.");
             },"AdvancedK9 delayed callout registration");
+            GameFiber.StartNew(CalloutMenuLoop,"AdvancedK9 callout menu");
+        }
+
+        private static void CalloutMenuLoop()
+        {
+            while(_running)
+            {
+                GameFiber.Yield();
+                if(Game.IsKeyDown(Keys.F7))
+                {
+                    _menuOpen=!_menuOpen;
+                    GameFiber.Wait(250);
+                }
+                if(!_menuOpen)continue;
+                Game.DisplayHelp("~b~AdvancedK9 Callouts~s~~n~~y~1~s~ Lost Child~n~~y~2~ Fugitive Trail~n~~y~3~ Armed Burglary~n~~y~F7~s~ Close",10);
+                if(Game.IsKeyDown(Keys.D1)){StartSelectedCallout("AdvancedK9: Lost Child");GameFiber.Wait(250);}
+                else if(Game.IsKeyDown(Keys.D2)){StartSelectedCallout("AdvancedK9: Fugitive Trail");GameFiber.Wait(250);}
+                else if(Game.IsKeyDown(Keys.D3)){StartSelectedCallout("AdvancedK9: Armed Burglary");GameFiber.Wait(250);}
+            }
+        }
+
+        private static void StartSelectedCallout(string name)
+        {
+            _menuOpen=false;
+            try
+            {
+                Game.LogTrivial("AdvancedK9 Callouts: menu requested "+name+".");
+                Functions.StartCallout(name);
+                Game.DisplayNotification("~b~AdvancedK9 Callouts:~s~ requested "+name+". Accept it through your normal LSPDFR/dispatch control.");
+            }
+            catch(Exception ex)
+            {
+                Game.LogTrivial("AdvancedK9 Callouts: menu could not start "+name+": "+ex);
+                Game.DisplayNotification("~r~AdvancedK9 Callouts:~s~ unable to start that callout. Check RagePluginHook.log.");
+            }
         }
 
         private static void InstallDependencyResolver()
@@ -51,6 +90,8 @@ namespace AdvancedK9.Callouts
 
         public override void Finally()
         {
+            _running=false;
+            _menuOpen=false;
             _registered=false;
             if(_dependencyResolver!=null){AppDomain.CurrentDomain.AssemblyResolve-=_dependencyResolver;_dependencyResolver=null;}
             Game.LogTrivial("AdvancedK9 Callouts: unloaded.");
