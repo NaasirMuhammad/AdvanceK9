89d691cab57743c51e8961f508c05274a672be25
NaasirMuhammad <141694761+NaasirMuhammad@users.noreply.github.com>
2026-09-15 13:39:03 -0400

    Prepare AdvancedK9 v0.23.3 core release

--- Bridge/Properties/AssemblyInfo.cs
+++ Bridge/Properties/AssemblyInfo.cs
@@ -2,8 +2,8 @@ using System.Reflection;
 
 [assembly: AssemblyTitle("AdvancedK9 LSPDFR Bridge")]
 [assembly: AssemblyDescription("LSPDFR AppDomain companion for AdvancedK9 Beta")]
-[assembly: AssemblyCompany("AdvancedK9 Community Project")]
+[assembly: AssemblyCompany("mtennekten")]
 [assembly: AssemblyProduct("AdvancedK9 LSPDFR Bridge Beta")]
-[assembly: AssemblyVersion("0.23.2.0")]
-[assembly: AssemblyFileVersion("0.23.2.0")]
-[assembly: AssemblyInformationalVersion("0.23.2-beta+bridge")]
+[assembly: AssemblyVersion("0.23.3.0")]
+[assembly: AssemblyFileVersion("0.23.3.0")]
+[assembly: AssemblyInformationalVersion("0.23.3-beta+bridge")]
